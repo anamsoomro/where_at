e946d92402c3ba6cb6b703a1f16beffd79c77217
anamsoomro <anamsoomroed@gmail.com>
2020-06-17 15:02:27 -0500

    maps in

---
+++ src/index.js
@@ -0,0 +1,31 @@
+console.log("working")
+console.log(api_key)
+
+
+function initMap () {
+  console.log("called");
+
+  const location = {lat: 51.9851, lng: 5.8987}
+
+  var panorama = new google.maps.StreetViewPanorama(
+    document.getElementById('view-map'), {
+      position: location,
+      pov: {
+        heading: 0,
+        pitch: 0
+      },
+      visible: true
+    });
+
+  var map = new google.maps.Map(
+    document.getElementById('guess-map'), {
+      zoom: 4,
+      center: location
+    });
+
+
+  
+
+}
+
+
